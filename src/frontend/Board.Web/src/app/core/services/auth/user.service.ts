5cabdbc0cb5549427b0f21c03d1a589296f0b5f7
SaWitC <ozabello21@gmail.com>
2025-09-09 20:39:00 +0300

    Add user profile component

---
+++ src/frontend/Board.Web/src/app/core/services/auth/user.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, Observable } from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UserService {
+    private permissionsSubject$$: BehaviorSubject<string[] | null> = new BehaviorSubject<string[] | null>(null);
+
+    constructor() { }
+
+    public get getPermissions() {
+        return this.permissionsSubject$$.value;
+    }
+
+    public emitPermissionsChanged(permissions: string[]) {
+        this.permissionsSubject$$.next(permissions)
+    }
+
+    public getPermissionsObservable$(): Observable<string[] | null> {
+        return this.permissionsSubject$$.asObservable();
+    }
+
+    public hasEditorPermission(): boolean {
+        return this.permissionsSubject$$.value?.includes('Editor') ?? false
+    }
+}
+
