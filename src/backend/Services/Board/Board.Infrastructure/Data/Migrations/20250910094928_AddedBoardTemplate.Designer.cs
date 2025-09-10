ea0df0ba41fa8ba16ed426c85100097d3096889b
DarkIceTea <barchuk.kanstantsin@innowise.com>
2025-09-10 13:52:26 +0200

    add migration for boardTemplate

--- src/backend/Services/Board/Board.Infrastructure/Data/Migrations/20250910094928_AddedBoardTemplate.Designer.cs
+++ src/backend/Services/Board/Board.Infrastructure/Data/Migrations/20250910094928_AddedBoardTemplate.Designer.cs
@@ -4,6 +4,7 @@ using Board.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Board.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(BoardDbContext))]
-    partial class BoardDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250910094928_AddedBoardTemplate")]
+    partial class AddedBoardTemplate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
