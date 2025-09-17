20266ef8622250ce1b06970917238b2abfa95f66
DarkIceTea <barchuk.kanstantsin@innowise.com>
2025-09-17 10:16:58 +0200

    Add localization for validators

--- src/backend/Services/Board/Board.Api/Features/Board/CreateBoard/CreateBoardValidator.cs
+++ src/backend/Services/Board/Board.Api/Features/Board/CreateBoard/CreateBoardValidator.cs
@@ -1,6 +1,9 @@
+using Board.Api.Resources;
 using Board.Application.DTOs;
 using FastEndpoints;
 using FluentValidation;
+using Microsoft.Extensions.Localization;
+using YamlDotNet.Serialization.BufferedDeserialization;
 
 namespace Board.Api.Features.Board.CreateBoard;
 
@@ -8,8 +11,17 @@ public class CreateBoardValidator : Validator<CreateBoardRequest>
 {
     public CreateBoardValidator()
     {
-        RuleFor(i => i.Title).NotNull().NotEmpty().MaximumLength(20);
-        RuleFor(i => i.Description).MaximumLength(100);
+        //RuleFor(i => i.Title)
+        //    .NotNull().NotEmpty().WithMessage(x => localizer["FieldIsRequired", x.Title])
+        //    .MaximumLength(20).WithMessage(x => localizer["MaxLengthExceeded", x.Title, 20]);
+
+        RuleFor(i => i.Title)
+            .NotNull().NotEmpty().WithMessage(x => string.Format(SharedResources.FieldIsRequired, nameof(x.Title)))
+            .MaximumLength(20).WithMessage(x => string.Format(SharedResources.MaxLengthExceeded, nameof(x.Title), 20));
+
+        RuleFor(i => i.Description).MaximumLength(100)
+            .WithMessage(x => string.Format(SharedResources.MaxLengthExceeded, nameof(x.Description), 100));
+
         RuleForEach(i => i.BoardUsers).NotNull().NotEmpty().SetValidator(new BoardUserValidator());
         //TODO ADD CHECK FOR OWNER
         RuleForEach(i => i.BoardColumns).NotNull().NotEmpty().SetValidator(new BoardColumnValidator());
@@ -20,8 +32,10 @@ public class BoardUserValidator : AbstractValidator<BoardUserDto>
 {
     public BoardUserValidator()
     {
-        RuleFor(c => c.Role).NotNull().NotEmpty();
-        RuleFor(c => c.Email).NotNull().NotEmpty().MaximumLength(100);
+        RuleFor(c => c.Role).NotNull().NotEmpty()
+            .WithMessage(x => string.Format(SharedResources.FieldIsRequired, nameof(x.Role)));
+        RuleFor(c => c.Email).NotNull().NotEmpty().MaximumLength(100)
+            .WithMessage(x => string.Format(SharedResources.MaxLengthExceeded, nameof(x.Email), 100));
     }
 }
 
@@ -29,7 +43,10 @@ public class BoardColumnValidator : AbstractValidator<BoardColumnDto>
 {
     public BoardColumnValidator()
     {
-        RuleFor(c => c.Title).NotNull().NotEmpty().MaximumLength(200);
-        RuleFor(c => c.Description).NotNull().NotEmpty().MaximumLength(10000);
+        RuleFor(c => c.Title).NotNull().NotEmpty()
+            .WithMessage(x => string.Format(SharedResources.FieldIsRequired, nameof(x.Title)))
+            .MaximumLength(200).WithMessage(x => string.Format(SharedResources.MaxLengthExceeded, nameof(x.Title), 200));
+        RuleFor(c => c.Description).NotNull().NotEmpty().WithMessage(x => string.Format(SharedResources.FieldIsRequired, nameof(x.Description)))
+            .MaximumLength(10000).WithMessage(x => string.Format(SharedResources.MaxLengthExceeded, nameof(x.Description), 10000));
     }
 }
--- src/backend/Services/Board/Board.Api/Features/BoardColumn/UpdateBoardColumn/UpdateBoardColumnValidator.cs
+++ src/backend/Services/Board/Board.Api/Features/BoardColumn/UpdateBoardColumn/UpdateBoardColumnValidator.cs
@@ -1,3 +1,4 @@
+using Board.Api.Resources;
 using FastEndpoints;
 using FluentValidation;
 
@@ -7,8 +8,18 @@ public class UpdateBoardColumnValidator : Validator<UpdateBoardColumnRequest>
 {
     public UpdateBoardColumnValidator()
     {
-        RuleFor(x => x.Id).NotEmpty();
-        RuleFor(x => x.Title).NotEmpty().MaximumLength(200);
-        RuleFor(x => x.Description).NotEmpty().MaximumLength(10000);
+        RuleFor(x => x.Id)
+            .NotEmpty()
+            .WithMessage(x => string.Format(SharedResources.FieldIsRequired, nameof(x.Id)));
+        RuleFor(x => x.Title)
+            .NotEmpty()
+            .WithMessage(x => string.Format(SharedResources.FieldIsRequired, nameof(x.Title)))
+            .MaximumLength(200)
+            .WithMessage(x => string.Format(SharedResources.MaxLengthExceeded, nameof(x.Title), 200));
+        RuleFor(x => x.Description)
+            .NotEmpty()
+            .WithMessage(x => string.Format(SharedResources.FieldIsRequired, nameof(x.Description)))
+            .MaximumLength(10000)
+            .WithMessage(x => string.Format(SharedResources.MaxLengthExceeded, nameof(x.Description), 10000));
     }
 }
