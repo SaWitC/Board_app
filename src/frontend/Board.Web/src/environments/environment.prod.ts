772bc524cdf3f157e71c8d1ee473932e746a1997
Sierrawolf87 <47673918+Sierrawolf87@users.noreply.github.com>
2025-09-26 17:10:10 +0200

    Update environment.prod.ts

--- src/frontend/Board.Web/src/environments/environment.prod.ts
+++ src/frontend/Board.Web/src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 import { Environment } from "./abstractions/environment";
 
 export const environment: Environment = {
-  apiUrl: 'https://localhost:7000/api',
+  apiUrl: 'https://api.inno-board.inno.ws/api',
   auth: {
     clientId: 'MpkBlYkrIbSbNlZaQneqWMPE5b0lGW6i',
     domain: 'dev-706vy0nkm3vbfojx.us.auth0.com',
     audience: 'inno-board-api-prod',
-    access_token_uri: 'https://localhost:7212/*',
+    access_token_uri: 'https://inno-board.inno.ws/*',
     isBypassAuthorization: false,
   },
 }
