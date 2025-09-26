573aeebaa8703e483353536ec91b7b34f614bfa8
Anton Volchkov <anton.volchkov@diflexmo.be>
2025-09-26 15:00:29 +0200

    Fix bug that users can not be deleted

--- src/frontend/Board.Web/src/app/pages/boards-section/boards-list/modals/create-board-modal/create-board-modal.component.ts
+++ src/frontend/Board.Web/src/app/pages/boards-section/boards-list/modals/create-board-modal/create-board-modal.component.ts
@@ -415,6 +415,7 @@ export class CreateBoardModalComponent implements OnInit {
 
   removeUser(index: number): void {
     this.boardUsers.removeAt(index);
+    this.boardUsers.markAsDirty();
   }
 
 
@@ -428,6 +429,7 @@ export class CreateBoardModalComponent implements OnInit {
 
   removeAdmin(index: number): void {
     this.boardAdmins.removeAt(index);
+    this.boardAdmins.markAsDirty();
   }
 
   //Empty board columns configuration
